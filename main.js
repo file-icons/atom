087b9fe06912d8827a62bd73d70b069c55e1d03b
Alhadis <gardnerjohng@gmail.com>
2016-11-10 14:24:00 +1100

    Add temporary fixes to enable use outside dev-mode
    Atom v1.12 is currently crashing whenever a window's opened in dev-mode.
    Until the regression gets sorted, we're forced to work in regular mode.
    
    References: atom/atom#13171

--- main.js
+++ main.js
@@ -49,3 +49,23 @@ module.exports = {
 		return FileRegistry.get(path).consumeIconClass();
 	}
 };
+
+
+
+/**
+ * Noop to suppress breakage at runtime.
+ *
+ * TODO: Delete once these PRs have all been publicly shipped -
+ *
+ *    atom/tree-view#967
+ *    atom/tabs/#392
+ *    atom/find-and-replace#802
+ *    atom/fuzzy-finder#262
+ *    atom/archive-view#40
+ *
+ * TODO: Don't forget to crank the minimum required Atom version, too.
+ */
+module.exports.onWillDeactivate = function(){
+	const {Disposable} = require("atom");
+	return new Disposable();
+};
