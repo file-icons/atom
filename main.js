78b87e61488479eacba1d706cca682f7ad1969e1
Alhadis <gardnerjohng@gmail.com>
2016-11-24 20:36:24 +1100

    Use a single entry-point to load consumer classes

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 "use strict";
 
+const Consumers      = require("./lib/consumers/all.js");
 const Options        = require("./lib/options.js");
 const FileRegistry   = require("./lib/file-registry.js");
 const IconRegistry   = require("./lib/icon-registry.js");
 const UI             = require("./lib/ui.js");
-
-const TreeView       = require("./lib/consumers/tree-view.js");
-const Tabs           = require("./lib/consumers/tabs.js");
-const FuzzyFinder    = require("./lib/consumers/fuzzy-finder.js");
-const FindAndReplace = require("./lib/consumers/find-and-replace.js");
-const ArchiveView    = require("./lib/consumers/archive-view.js");
 
 
 module.exports = {
@@ -17,11 +12,7 @@ module.exports = {
 	activate(){
 		Options.init();
 		UI.init();
-		TreeView.init();
-		Tabs.init();
-		FuzzyFinder.init();
-		FindAndReplace.init();
-		ArchiveView.init();
+		Consumers.init();
 		FileRegistry.init();
 		IconRegistry.init();
 		UI.observe();
@@ -32,11 +23,7 @@ module.exports = {
 	},
 	
 	deactivate(){
-		TreeView.reset();
-		Tabs.reset();
-		ArchiveView.reset();
-		FindAndReplace.reset();
-		FuzzyFinder.reset();
+		Consumers.reset();
 		FileRegistry.reset();
 		IconRegistry.reset();
 		UI.reset();
