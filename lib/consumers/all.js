file-icons/atom

commit 9a5266ee1f3a07fc3c297d99756fdaab825fab84

Avoid patching Nuclide if ANY icon-service is supported
At the rate facebook/nuclide#951 is going, we'd be lucky if they support
any icon service whatsoever, let alone ours.

lib/consumers/all.js
@@ -45,6 +45,11 @@ class ConsumerManager{
 		if(!pkg || !pkg.metadata) return true;
 		
 		const services = pkg.metadata.consumedServices;
+		
+		// For Nuclide, anything is better than nothing. Take whatever we can get.
+		if("nuclide" === packageName && services["atom.file-icons"])
+			return false;
+		
 		return !(services && services["file-icons.element-icons"]);
 	}
 	

commit a7bcca7a1b0d21c250e52168f900642fcaa0c07b

Fix regression introduced by 9a5266e

lib/consumers/all.js
@@ -47,7 +47,7 @@ class ConsumerManager{
 		const services = pkg.metadata.consumedServices;
 		
 		// For Nuclide, anything is better than nothing. Take whatever we can get.
-		if("nuclide" === packageName && services["atom.file-icons"])
+		if("nuclide" === packageName && services && services["atom.file-icons"])
 			return false;
 		
 		return !(services && services["file-icons.element-icons"]);