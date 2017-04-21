file-icons/atom

commit b3051db1d3d54196d303329285338d0e00120b80

Release tabs when switching projects in Project Plus
References: #550

lib/consumers/tabs.js
@@ -42,10 +42,19 @@ class Tabs extends Consumer {
 				this.add(tab);
 			})
 		);
-	
-		for(const bar of this.packageModule.tabBarViews)
-			for(const tab of bar.getTabs())
-				this.add(tab);
+		this.addCurrentTabs();
+		
+		// Project Plus has a "switch" feature that flips between workspaces
+		// in the same window. Make sure Tabs are cleared when doing so.
+		if(atom.packages.activePackages["project-plus"]){
+			const util = this.loadPackageFile("lib/util.js");
+			this.punch(util, "switchToProject", oldFn => {
+				this.resetNodes();
+				const switchPromise = oldFn();
+				switchPromise.then(() => this.addCurrentTabs());
+				return switchPromise;
+			});
+		}
 	}
 	
 	
@@ -56,12 +65,18 @@ class Tabs extends Consumer {
 	
 	
 	reset(){
-		this.tabs.forEach(tab => this.remove(tab));
+		this.resetNodes();
 		super.reset();
-		this.tabs.clear();
 		this.tabs = null;
 		this.tabElements = null;
 		this.tabsByElement = null;
+	}
+	
+	
+	resetNodes(){
+		super.resetNodes();
+		this.tabs.forEach(tab => this.remove(tab));
+		this.tabs.clear();
 	}
 	
 	
@@ -78,6 +93,13 @@ class Tabs extends Consumer {
 			});
 			this.disposables.add(disposable);
 		}
+	}
+	
+	
+	addCurrentTabs(){
+		for(const bar of this.packageModule.tabBarViews)
+			for(const tab of bar.getTabs())
+				this.add(tab);
 	}
 	
 	

commit 6f99f53e88cb92bd1bc87dee94dba2b809414dff

Fix unhandled exception caused by b3051db
Resolves #590.

lib/consumers/tabs.js
@@ -46,15 +46,18 @@ class Tabs extends Consumer {
 		
 		// Project Plus has a "switch" feature that flips between workspaces
 		// in the same window. Make sure Tabs are cleared when doing so.
-		if(atom.packages.activePackages["project-plus"]){
-			const util = this.loadPackageFile("lib/util.js");
-			this.punch(util, "switchToProject", oldFn => {
-				this.resetNodes();
-				const switchPromise = oldFn();
-				switchPromise.then(() => this.addCurrentTabs());
-				return switchPromise;
-			});
-		}
+		if(atom.packages.activePackages["project-plus"])
+			try{
+				const util = this.loadPackageFile("lib/util.js");
+				this.punch(util, "switchToProject", oldFn => {
+					this.resetNodes();
+					const switchPromise = oldFn();
+					switchPromise.then(() => this.addCurrentTabs());
+					return switchPromise;
+				});
+			} catch(error){
+				console.error("FILE-ICONS: Error patching tabs for project-plus", error);
+			}
 	}
 	
 	