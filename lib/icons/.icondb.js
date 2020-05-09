file-icons/atom

commit fbd86e51c9245c431df780126220ab2312d6dd14

Implement icon-tags to reduce lookup overhead
As suggested in #815, we can achieve faster startup time by precomputing
icon indexes instead of performing a potentially-expensive lookup.

Resolves: file-icons/atom#815

config.cson
@@ -869,7 +869,7 @@ fileIcons:
 	Binary:
 		icon: "binary"
 		match: [
-			[/\.([ls]?o|out)$/i, "medium-red"]
+			[/\.([ls]?o|out)$/i, "medium-red", tag: "binary"]
 			[".axf", "dark-blue"]
 			[".elf", "medium-green"]
 			[".la", "dark-red"]
@@ -1323,7 +1323,7 @@ fileIcons:
 	CoffeeScript:
 		icon: "coffee"
 		match: [
-			[".coffee", "medium-maroon", scope: "coffee", alias: /^Coffee(-Script)?$/i, interpreter: "coffee"]
+			[".coffee", "medium-maroon", scope: "coffee", alias: /^Coffee(-Script)?$/i, interpreter: "coffee", tag: "coffee"]
 			[".cjsx", "dark-maroon"]
 			[".coffee.ecr", "medium-cyan", priority: 2]
 			[".coffee.erb", "medium-red",  priority: 2]
@@ -2694,6 +2694,7 @@ fileIcons:
 		match: ".go"
 		colour: "medium-blue"
 		scope: /\.go(template)?$/i
+		tag: "go"
 
 	Godot:
 		icon: "godot"
@@ -2826,7 +2827,7 @@ fileIcons:
 	Haskell:
 		icon: "haskell"
 		match: [
-			[".hs", "medium-purple", scope: "source.haskell", interpreter: "runhaskell"]
+			[".hs", "medium-purple", scope: "source.haskell", interpreter: "runhaskell", tag: "haskell"]
 			[".hsc", "medium-blue", scope: "hsc2hs"]
 			[".c2hs", "dark-purple", scope: "c2hs", alias: /^C2hs(\s*Haskell)?$/i]
 			[".lhs", "dark-blue", scope: "latex.haskell", alias: /^(lhaskell|lhs|Literate\s*Haskell)$/i]
@@ -3145,7 +3146,7 @@ fileIcons:
 	JavaScript:
 		icon: "js"
 		match: [
-			[".js", "auto-yellow", scope: "js", alias: /^(js|node)$/i, interpreter: /^(node|chakra|d8|iojs|g?js|qjs(c?|bnc?)?|mujs|rhino|slimerjs|v8(-shell)?)$/]
+			[".js", "auto-yellow", scope: "js", alias: /^(js|node)$/i, interpreter: /^(node|chakra|d8|iojs|g?js|qjs(c?|bnc?)?|mujs|rhino|slimerjs|v8(-shell)?)$/, tag: "js"]
 			["._js", "auto-orange"]
 			[".jsb", "auto-maroon"]
 			[".jsm", "auto-blue"]
@@ -4645,7 +4646,7 @@ fileIcons:
 	Perl:
 		icon: "perl"
 		match: [
-			[/\.p(er)?l$/i, "medium-blue", alias: /^C?Perl$/i, scope: "perl", interpreter: /^c?perl5?$/]
+			[/\.p(er)?l$/i, "medium-blue", alias: /^C?Perl$/i, scope: "perl", interpreter: /^c?perl5?$/, tag: "perl"]
 			[".al", "dark-blue"]
 			[".ph", "dark-purple"]
 			[".plx", "medium-purple"]
@@ -4954,7 +4955,7 @@ fileIcons:
 	Python:
 		icon: "python"
 		match: [
-			[".py", "dark-blue", scope: "python", alias: "rusthon", interpreter: /python[\d.]*/]
+			[".py", "dark-blue", scope: "python", alias: "rusthon", interpreter: /python[\d.]*/, tag: "python"]
 			[".ipy", "medium-blue"]
 			[".isolate", "dark-green"]
 			[".pep", "medium-orange", "pep8"]
@@ -5282,6 +5283,7 @@ fileIcons:
 		icon: "ruby"
 		match: [
 			[/\.(rb|ru|ruby|erb|gemspec|god|mspec|pluginspec|podspec|rabl|rake|opal)$/i, "medium-red"
+				tag: "ruby"
 				scope: "ruby"
 				alias: /^(rbx?|rake|jruby|macruby)$/i
 				interpreter: /(mac|j)?ruby|rake|rbx/]
@@ -5301,7 +5303,7 @@ fileIcons:
 	Rust:
 		icon: "rust"
 		match: [
-			[".rs", "medium-maroon", "rust"]
+			[".rs", "medium-maroon", scope: "rust", interpreter: "rust", alias: "rust", tag: "rust"]
 			[".rlib", "light-maroon"]
 			[/^rust-toolchain$/, "medium-red"]
 		]
@@ -5479,6 +5481,7 @@ fileIcons:
 		generic: true
 		match: [
 			[/\.(sh|rc|bats|bash|tool|install|command)$/i, "medium-purple"
+				tag: "executable"
 				scope: "shell"
 				alias: /^(sh|shell|Shell-?Script|Bash)$/i
 				interpreter: /^([bd]ash|a?sh|zsh|rc)$/]
@@ -5950,12 +5953,14 @@ fileIcons:
 		icon: "test-coffee"
 		match: /[._-](spec|test)s?\.((lit)?coffee|iced|cjsx)$/i
 		colour: "auto-maroon"
+		tag: "coffeeTest"
 		priority: 1.5
 
 	"Test, Generic":
 		icon: "test-generic"
 		match: [
-			[/([\\/])t\1t?\d+(?:(?!\1).)+\.sh$|[._-](spec|test)s?\.sh$/i, "medium-green", matchPath: true, priority: 1.5]
+			[/[._-](spec|test)s?\.sh$/i, "medium-green", priority: 1.5, tag: "genericTest"]
+			[/([\\/])t\1t?\d+(?:(?!\1).)+\.sh$/i, "medium-green", matchPath: true, priority: 1.5]
 			[".test", "medium-green", priority: 0.25]
 			[".xspec", "dark-green"]
 		]
@@ -5964,19 +5969,21 @@ fileIcons:
 		icon: "test-go"
 		match: /[._-](spec|test)s?\.go$/i
 		colour: "auto-cyan"
+		tag: "goTest"
 		priority: 1.5
 
 	"Test, Haskell":
 		icon: "test-hs"
 		match: /[._-](spec|test)s?\.(hsc?|c2hs|lhs)$/i
 		colour: "auto-purple"
+		tag: "haskellTest"
 		priority: 1.5
 
 	"Test, JavaScript":
 		icon: "test-js"
 		priority: 1.5
 		match: [
-			[/[._-](spec|test)s?\.([_s]?js|js[bms]|es\d*)$/i, "auto-yellow"]
+			[/[._-](spec|test)s?\.([_s]?js|js[bms]|es\d*)$/i, "auto-yellow", tag: "jsTest"]
 			[/[._-](spec|test)s?\.mjs$/i, "auto-blue"]
 			[/[._-](spec|test)s?\.cjs$/i, "auto-green"]
 			[/([\\\/])(?:(spec|test)s?|t)\1(?:\d+[-.])+(?!-)[^.\\\/]+\.js$/i, "auto-yellow", matchPath: true]
@@ -5987,7 +5994,7 @@ fileIcons:
 	"Test, Perl":
 		icon: "test-perl"
 		match: [
-			[".t", "auto-blue"]
+			[".t", "auto-blue", tag: "perlTest"]
 			[/([\\\x2F])t\1(?:(?!\1).)+\.t$/i, "auto-blue", priority: 1.5, matchPath: true]
 			[/([\\\x2F])(test|spec)s?(\1((?!\1).)+)*\1((?!\1).)+[._-](spec|test)s?\.p(er)?l$/i, "auto-blue", priority: 1.5, matchPath: true]
 		]
@@ -5995,7 +6002,7 @@ fileIcons:
 	"Test, Python":
 		icon: "test-python"
 		match: [
-			[/[._-](spec|test)s?\.py(3|thon)?/i, "dark-blue", priority: 1.5]
+			[/[._-](spec|test)s?\.py(3|thon)?/i, "dark-blue", priority: 1.5, tag: "pythonTest"]
 			[/([\\\x2F])(test|spec)s?(\1((?!\1).)+)*\1(((?!\1).)+[._-](?:spec|test)s?|(?:spec|test)s?[._-].+)\.py(3|thon)?$/i, "dark-blue", priority: 1.5, matchPath: true]
 		]
 
@@ -6010,7 +6017,7 @@ fileIcons:
 	"Test, Ruby":
 		icon: "test-ruby"
 		match: [
-			[/[._-](spec|test)s?\.(rb|ruby)$/i, "auto-red", priority: 2]
+			[/[._-](spec|test)s?\.(rb|ruby)$/i, "auto-red", priority: 2, tag: "rubyTest"]
 			[/([\\\x2F])(t|tests?|specs?)\1+(?:(?!\1).)*\.(rb|ruby)$/, "auto-red", priority: 2, matchPath: true]
 		]
 
@@ -6018,13 +6025,14 @@ fileIcons:
 		icon: "test-rust"
 		match: /[._-](spec|test)s?\.rs$/i
 		colour: "auto-maroon"
+		tag: "rustTest"
 		priority: 1.5
 
 	"Test, TypeScript":
 		icon: "test-ts"
 		priority: 1.5
 		match: [
-			[/[._-](spec|test)s?\.ts$/i, "auto-blue"]
+			[/[._-](spec|test)s?\.ts$/i, "auto-blue", tag: "tsTest"]
 			[/[._-](spec|test)s?\.tsx$/i, ["light-blue", "medium-blue"]]
 			[/([\\\/])(spec|test)s?\1(?:\d+[-.])+(?!-)[^.\\\/]+\.ts$/i, "auto-blue", matchPath: true]
 			[/([\\\/])(spec|test)s?\1(?:\d+[-.])+(?!-)[^.\\\/]+\.tsx$/i, ["light-blue", "medium-blue"], matchPath: true]
@@ -6220,6 +6228,7 @@ fileIcons:
 		match: ".ts"
 		colour: "medium-blue"
 		interpreter: /\b(deno|tsc|ts-node)$/
+		tag: "ts"
 
 	Typings:
 		icon: "typings"

commit 33b2c556a2904598201ccd332bc88e32ba36b0d1

Fix daa9906 not applying to shell-scripts
Regression introduced by careless refactoring in fbd86e5.

config.cson
@@ -5977,8 +5977,7 @@ fileIcons:
 	"Test, Generic":
 		icon: "test-generic"
 		match: [
-			[/[._-](spec|test)s?\.sh$/i, "medium-green", priority: 1.5, tag: "genericTest"]
-			[/([\\/])t\1t?\d+(?:(?!\1).)+\.sh$/i, "medium-green", matchPath: true, priority: 1.5]
+			[/([\\/])t\1t?\d+(?:(?!\1).)+\.sh$|[._-](spec|test)s?\.sh$/i, "medium-green", matchPath: true, priority: 1.5, tag: "genericTest"]
 			[".test", "medium-green", priority: 0.25]
 			[".xspec", "dark-green"]
 		]

lib/icons/.icondb.js
@@ -379,8 +379,7 @@ module.exports = [
 ["moho-icon",["medium-orange","medium-orange"],/\.mohoproj$/i,1.5],
 ["pros-icon",["medium-orange","medium-orange"],/^project\.pros$/i,1.5],
 ["test-coffee-icon",["medium-maroon","dark-maroon"],/[._-](?:spec|test)s?\.(?:(?:lit)?coffee|iced|cjsx)$/i,1.5],
-["test-generic-icon",["medium-green","medium-green"],/[._-](?:spec|test)s?\.sh$/i,1.5],
-["test-generic-icon",["medium-green","medium-green"],/([\\\/])t\1t?\d+(?:(?!\1).)+\.sh$/i,1.5,true],
+["test-generic-icon",["medium-green","medium-green"],/([\\\/])t\1t?\d+(?:(?!\1).)+\.sh$|[._-](spec|test)s?\.sh$/i,1.5,true],
 ["test-go-icon",["medium-cyan","dark-cyan"],/[._-](?:spec|test)s?\.go$/i,1.5],
 ["test-hs-icon",["medium-purple","dark-purple"],/[._-](?:spec|test)s?\.(?:hsc?|c2hs|lhs)$/i,1.5],
 ["test-js-icon",["medium-yellow","dark-yellow"],/[._-](?:spec|test)s?\.(?:[_s]?js|js[bms]|es\d*)$/i,1.5],
@@ -1902,5 +1901,5 @@ module.exports = [
 ["book-icon",["dark-blue","dark-blue"],/^zork\d\.(?!pre$|pur$)[a-z]+$/,0],
 ["key-icon",["dark-blue","dark-blue"],/\.ssh[\/\\][^\/\\\s]+$/,0,true],
 ["v8-icon",["medium-blue","medium-blue"],/^(?:[dv]8|v8[-_.][^.]*|mksnapshot|mkpeephole)$/i,0]],
-[[151,161,162,163,288,324,361,385,393,406,410,412,437,460,464,465,466,467,472,473,474,475,480,489,493,512,539,540,555,560,567,572,574,579,608,624,629,630,634,638,643,647,648,654,663,668,693,698,701,710,719,729,730,734,735,743,771,789,794,798,812,816,821,823,825,826,829,831,832,837,840,843,844,862,865,887,891,892,897,917,918,921,922,924,925,927,928,930,976,977,980,986,1003,1004,1005,1013,1016,1018,1031,1042,1044,1048,1056,1057,1065,1068,1072,1074,1081,1082,1103,1124,1131,1187,1189,1192,1205,1214,1222,1223,1235,1238,1242,1244,1247,1257,1258,1260,1275,1280,1281,1284,1321,1324,1337,1338,1339,1345,1350,1351,1363,1364,1378,1385,1387,1390,1393,1394,1397,1398,1401,1417,1420,1421,1424,1425,1448,1454,1464,1467,1471,1472,1478,1482,1487,1493,1494,1496,1498,1501,1516,1518,1521,1523,1530,1548,1551,1553,1576,1579,1583,1592,1606,1629,1630,1631,1637,1667,1669,1673,1688,1699,1701,1719,1745,1749,1752,1756,1757,1796,1798,1802,1811,1812,1835,1840],[9,12,15,60,87,123,161,163,167,185,190,200,258,268,277,286,323,324,354,356,357,360,362,367,370,385,389,393,397,399,400,404,406,408,409,410,412,414,415,416,424,428,429,430,432,433,434,436,437,439,460,462,463,464,465,466,467,472,473,474,475,480,482,489,493,501,502,504,505,511,512,520,521,522,526,529,533,538,553,554,555,558,559,560,565,567,569,572,574,576,577,579,600,608,615,617,622,623,624,625,627,628,629,630,631,632,634,635,636,638,639,642,643,644,647,648,654,655,658,663,666,668,671,674,679,681,683,685,690,692,699,701,706,707,708,709,710,713,714,715,716,718,719,726,729,730,733,734,735,741,743,747,748,771,772,774,775,778,783,784,785,786,788,789,790,791,793,794,797,798,820,826,832,843,844,845,848,855,862,865,866,867,872,873,877,880,883,887,888,891,892,894,895,897,898,899,900,902,912,913,916,918,922,924,925,927,928,930,931,932,953,968,969,971,974,976,978,980,981,984,986,1001,1003,1004,1005,1007,1008,1011,1012,1013,1015,1016,1017,1018,1019,1022,1025,1029,1031,1034,1036,1037,1042,1044,1048,1053,1056,1057,1062,1065,1068,1070,1072,1074,1081,1082,1092,1093,1094,1097,1103,1115,1124,1131,1132,1182,1187,1189,1191,1192,1196,1205,1206,1208,1210,1214,1220,1222,1229,1230,1231,1233,1235,1236,1237,1238,1242,1244,1245,1247,1250,1251,1252,1257,1258,1260,1268,1269,1270,1273,1275,1280,1281,1282,1284,1285,1286,1287,1288,1289,1290,1307,1313,1316,1317,1318,1319,1320,1322,1323,1324,1334,1337,1338,1339,1344,1345,1350,1353,1355,1361,1362,1363,1364,1366,1369,1378,1384,1385,1388,1389,1390,1393,1394,1396,1397,1398,1401,1408,1412,1413,1414,1417,1420,1421,1424,1425,1434,1435,1440,1448,1454,1457,1462,1463,1464,1467,1468,1469,1471,1472,1478,1482,1485,1487,1490,1491,1492,1493,1494,1496,1498,1501,1504,1508,1516,1519,1521,1523,1524,1530,1533,1538,1539,1540,1541,1543,1544,1546,1548,1549,1551,1553,1565,1566,1575,1577,1578,1579,1583,1586,1588,1589,1592,1593,1594,1596,1606,1612,1614,1620,1624,1626,1629,1630,1631,1637,1645,1646,1660,1661,1663,1664,1665,1666,1667,1668,1669,1671,1673,1682,1684,1688,1693,1697,1698,1699,1701,1719,1726,1744,1745,1746,1747,1749,1753,1756,1757,1759,1767,1768,1792,1793,1795,1796,1798,1802,1804,1806,1809,1811,1812,1835,1837,1838,1839,1849],[0,10,11,16,85,274,287,310,311,336,342,343,345,347,351,352,519,609,610,611,612,667,669,670,998,1473,1525,1634,1815,1816,1856],[2,9,12,15,60,87,123,161,163,167,185,190,200,258,268,277,286,310,323,324,354,356,357,360,362,367,370,385,389,393,397,399,400,404,406,408,409,410,412,414,415,416,424,428,429,430,432,433,434,436,437,439,460,462,463,464,465,466,467,472,473,474,475,480,482,489,493,501,502,504,505,511,512,520,521,522,526,529,533,538,539,540,553,554,555,558,559,560,565,567,569,572,574,576,577,579,600,601,604,608,613,615,617,622,623,624,625,627,628,629,630,631,632,634,635,636,638,639,642,643,644,647,648,653,654,655,657,658,659,663,666,668,671,674,679,681,683,685,690,692,699,701,706,707,708,709,710,713,714,715,716,718,719,726,729,730,733,734,735,741,743,747,748,766,771,772,774,775,778,783,784,785,786,788,789,790,791,793,794,797,798,811,812,816,819,820,821,823,825,826,829,830,831,832,833,836,837,840,843,844,845,848,855,862,865,866,867,872,873,877,880,883,887,888,891,892,894,895,897,898,899,900,902,912,913,916,917,918,919,921,922,924,925,927,928,930,931,932,953,968,969,971,974,976,978,980,981,984,986,1001,1003,1004,1005,1007,1008,1011,1012,1013,1015,1016,1017,1018,1019,1022,1025,1029,1031,1034,1036,1037,1042,1044,1048,1053,1056,1057,1062,1065,1068,1070,1072,1074,1081,1082,1092,1093,1094,1097,1103,1115,1124,1131,1132,1182,1187,1189,1191,1192,1194,1195,1196,1205,1206,1208,1210,1214,1220,1222,1229,1230,1231,1233,1235,1236,1237,1238,1242,1244,1245,1247,1250,1251,1252,1257,1258,1260,1268,1269,1270,1273,1275,1280,1281,1282,1284,1285,1286,1287,1288,1289,1290,1307,1313,1316,1317,1318,1319,1320,1322,1323,1324,1334,1337,1338,1339,1344,1345,1350,1353,1355,1361,1362,1363,1364,1366,1369,1378,1384,1385,1388,1389,1390,1393,1394,1396,1397,1398,1401,1408,1412,1413,1414,1417,1420,1421,1424,1425,1434,1435,1440,1448,1454,1457,1462,1463,1464,1467,1468,1469,1471,1472,1478,1482,1485,1487,1490,1491,1492,1493,1494,1496,1498,1501,1504,1508,1510,1516,1518,1519,1521,1523,1524,1530,1533,1538,1539,1540,1541,1543,1544,1546,1548,1549,1551,1553,1554,1565,1566,1575,1577,1578,1579,1583,1586,1588,1589,1592,1593,1594,1596,1606,1612,1614,1620,1624,1626,1629,1630,1631,1637,1641,1645,1646,1660,1661,1663,1664,1665,1666,1667,1668,1669,1671,1673,1682,1684,1688,1693,1697,1698,1699,1701,1719,1726,1744,1745,1746,1747,1749,1753,1756,1757,1759,1767,1768,1792,1793,1795,1796,1798,1802,1804,1806,1809,1811,1812,1815,1832,1835,1837,1838,1839,1849],[0,3,4,276,325,363,380,390,410,443,444,445,446,447,448,449,450,451,452,454,455,456,537,582,583,584,585,586,589,590,591,595,597,604,619,656,685,747,755,756,757,758,759,760,763,764,766,770,810,811,837,840,905,906,916,935,936,937,938,939,940,941,942,943,944,945,946,947,948,950,951,952,953,954,955,956,958,959,961,962,965,1151,1203,1227,1312,1336,1345,1350,1366,1369,1451,1453,1471,1532,1562,1632,1690,1691,1692,1694,1708,1709,1710,1711,1712,1713,1714,1715,1716,1717,1718,1720,1724,1753,1838,1850,1851]],
-{binary:483,coffee:572,go:866,haskell:897,js:986,perl:1339,python:1398,ruby:1478,rust:1482,executable:1516,coffeeTest:334,genericTest:335,goTest:337,haskellTest:338,jsTest:339,perlTest:1623,pythonTest:344,rubyTest:273,rustTest:348,tsTest:349,ts:1669}]];
+[[151,161,162,163,288,324,360,384,392,405,409,411,436,459,463,464,465,466,471,472,473,474,479,488,492,511,538,539,554,559,566,571,573,578,607,623,628,629,633,637,642,646,647,653,662,667,692,697,700,709,718,728,729,733,734,742,770,788,793,797,811,815,820,822,824,825,828,830,831,836,839,842,843,861,864,886,890,891,896,916,917,920,921,923,924,926,927,929,975,976,979,985,1002,1003,1004,1012,1015,1017,1030,1041,1043,1047,1055,1056,1064,1067,1071,1073,1080,1081,1102,1123,1130,1186,1188,1191,1204,1213,1221,1222,1234,1237,1241,1243,1246,1256,1257,1259,1274,1279,1280,1283,1320,1323,1336,1337,1338,1344,1349,1350,1362,1363,1377,1384,1386,1389,1392,1393,1396,1397,1400,1416,1419,1420,1423,1424,1447,1453,1463,1466,1470,1471,1477,1481,1486,1492,1493,1495,1497,1500,1515,1517,1520,1522,1529,1547,1550,1552,1575,1578,1582,1591,1605,1628,1629,1630,1636,1666,1668,1672,1687,1698,1700,1718,1744,1748,1751,1755,1756,1795,1797,1801,1810,1811,1834,1839],[9,12,15,60,87,123,161,163,167,185,190,200,258,268,277,286,323,324,353,355,356,359,361,366,369,384,388,392,396,398,399,403,405,407,408,409,411,413,414,415,423,427,428,429,431,432,433,435,436,438,459,461,462,463,464,465,466,471,472,473,474,479,481,488,492,500,501,503,504,510,511,519,520,521,525,528,532,537,552,553,554,557,558,559,564,566,568,571,573,575,576,578,599,607,614,616,621,622,623,624,626,627,628,629,630,631,633,634,635,637,638,641,642,643,646,647,653,654,657,662,665,667,670,673,678,680,682,684,689,691,698,700,705,706,707,708,709,712,713,714,715,717,718,725,728,729,732,733,734,740,742,746,747,770,771,773,774,777,782,783,784,785,787,788,789,790,792,793,796,797,819,825,831,842,843,844,847,854,861,864,865,866,871,872,876,879,882,886,887,890,891,893,894,896,897,898,899,901,911,912,915,917,921,923,924,926,927,929,930,931,952,967,968,970,973,975,977,979,980,983,985,1000,1002,1003,1004,1006,1007,1010,1011,1012,1014,1015,1016,1017,1018,1021,1024,1028,1030,1033,1035,1036,1041,1043,1047,1052,1055,1056,1061,1064,1067,1069,1071,1073,1080,1081,1091,1092,1093,1096,1102,1114,1123,1130,1131,1181,1186,1188,1190,1191,1195,1204,1205,1207,1209,1213,1219,1221,1228,1229,1230,1232,1234,1235,1236,1237,1241,1243,1244,1246,1249,1250,1251,1256,1257,1259,1267,1268,1269,1272,1274,1279,1280,1281,1283,1284,1285,1286,1287,1288,1289,1306,1312,1315,1316,1317,1318,1319,1321,1322,1323,1333,1336,1337,1338,1343,1344,1349,1352,1354,1360,1361,1362,1363,1365,1368,1377,1383,1384,1387,1388,1389,1392,1393,1395,1396,1397,1400,1407,1411,1412,1413,1416,1419,1420,1423,1424,1433,1434,1439,1447,1453,1456,1461,1462,1463,1466,1467,1468,1470,1471,1477,1481,1484,1486,1489,1490,1491,1492,1493,1495,1497,1500,1503,1507,1515,1518,1520,1522,1523,1529,1532,1537,1538,1539,1540,1542,1543,1545,1547,1548,1550,1552,1564,1565,1574,1576,1577,1578,1582,1585,1587,1588,1591,1592,1593,1595,1605,1611,1613,1619,1623,1625,1628,1629,1630,1636,1644,1645,1659,1660,1662,1663,1664,1665,1666,1667,1668,1670,1672,1681,1683,1687,1692,1696,1697,1698,1700,1718,1725,1743,1744,1745,1746,1748,1752,1755,1756,1758,1766,1767,1791,1792,1794,1795,1797,1801,1803,1805,1808,1810,1811,1834,1836,1837,1838,1848],[0,10,11,16,85,274,287,310,311,335,341,342,344,346,350,351,518,608,609,610,611,666,668,669,997,1472,1524,1633,1814,1815,1855],[2,9,12,15,60,87,123,161,163,167,185,190,200,258,268,277,286,310,323,324,353,355,356,359,361,366,369,384,388,392,396,398,399,403,405,407,408,409,411,413,414,415,423,427,428,429,431,432,433,435,436,438,459,461,462,463,464,465,466,471,472,473,474,479,481,488,492,500,501,503,504,510,511,519,520,521,525,528,532,537,538,539,552,553,554,557,558,559,564,566,568,571,573,575,576,578,599,600,603,607,612,614,616,621,622,623,624,626,627,628,629,630,631,633,634,635,637,638,641,642,643,646,647,652,653,654,656,657,658,662,665,667,670,673,678,680,682,684,689,691,698,700,705,706,707,708,709,712,713,714,715,717,718,725,728,729,732,733,734,740,742,746,747,765,770,771,773,774,777,782,783,784,785,787,788,789,790,792,793,796,797,810,811,815,818,819,820,822,824,825,828,829,830,831,832,835,836,839,842,843,844,847,854,861,864,865,866,871,872,876,879,882,886,887,890,891,893,894,896,897,898,899,901,911,912,915,916,917,918,920,921,923,924,926,927,929,930,931,952,967,968,970,973,975,977,979,980,983,985,1000,1002,1003,1004,1006,1007,1010,1011,1012,1014,1015,1016,1017,1018,1021,1024,1028,1030,1033,1035,1036,1041,1043,1047,1052,1055,1056,1061,1064,1067,1069,1071,1073,1080,1081,1091,1092,1093,1096,1102,1114,1123,1130,1131,1181,1186,1188,1190,1191,1193,1194,1195,1204,1205,1207,1209,1213,1219,1221,1228,1229,1230,1232,1234,1235,1236,1237,1241,1243,1244,1246,1249,1250,1251,1256,1257,1259,1267,1268,1269,1272,1274,1279,1280,1281,1283,1284,1285,1286,1287,1288,1289,1306,1312,1315,1316,1317,1318,1319,1321,1322,1323,1333,1336,1337,1338,1343,1344,1349,1352,1354,1360,1361,1362,1363,1365,1368,1377,1383,1384,1387,1388,1389,1392,1393,1395,1396,1397,1400,1407,1411,1412,1413,1416,1419,1420,1423,1424,1433,1434,1439,1447,1453,1456,1461,1462,1463,1466,1467,1468,1470,1471,1477,1481,1484,1486,1489,1490,1491,1492,1493,1495,1497,1500,1503,1507,1509,1515,1517,1518,1520,1522,1523,1529,1532,1537,1538,1539,1540,1542,1543,1545,1547,1548,1550,1552,1553,1564,1565,1574,1576,1577,1578,1582,1585,1587,1588,1591,1592,1593,1595,1605,1611,1613,1619,1623,1625,1628,1629,1630,1636,1640,1644,1645,1659,1660,1662,1663,1664,1665,1666,1667,1668,1670,1672,1681,1683,1687,1692,1696,1697,1698,1700,1718,1725,1743,1744,1745,1746,1748,1752,1755,1756,1758,1766,1767,1791,1792,1794,1795,1797,1801,1803,1805,1808,1810,1811,1814,1831,1834,1836,1837,1838,1848],[0,3,4,276,325,362,379,389,409,442,443,444,445,446,447,448,449,450,451,453,454,455,536,581,582,583,584,585,588,589,590,594,596,603,618,655,684,746,754,755,756,757,758,759,762,763,765,769,809,810,836,839,904,905,915,934,935,936,937,938,939,940,941,942,943,944,945,946,947,949,950,951,952,953,954,955,957,958,960,961,964,1150,1202,1226,1311,1335,1344,1349,1365,1368,1450,1452,1470,1531,1561,1631,1689,1690,1691,1693,1707,1708,1709,1710,1711,1712,1713,1714,1715,1716,1717,1719,1723,1752,1837,1849,1850]],
+{binary:482,coffee:571,go:865,haskell:896,js:985,perl:1338,python:1397,ruby:1477,rust:1481,executable:1515,coffeeTest:334,genericTest:335,goTest:336,haskellTest:337,jsTest:338,perlTest:1622,pythonTest:343,rubyTest:273,rustTest:347,tsTest:348,ts:1668}]];

lib/service/strategies/hashbang-strategy.js
@@ -17,16 +17,16 @@ class HashbangStrategy extends HeaderStrategy {
 	
 	getRemapping(icon){
 		switch(icon){
-			case taggedIcons.coffee:   return taggedIcons.coffeeTest;
-			case taggedIcons.go:       return taggedIcons.goTest;
-			case taggedIcons.haskell:  return taggedIcons.haskellTest;
-			case taggedIcons.js:       return taggedIcons.jsTest;
-			case taggedIcons.perl:     return taggedIcons.perlTest;
-			case taggedIcons.python:   return taggedIcons.pythonTest;
-			case taggedIcons.ruby:     return taggedIcons.rubyTest;
-			case taggedIcons.rust:     return taggedIcons.rustTest;
-			case taggedIcons.sh:       return taggedIcons.genericTest;
-			case taggedIcons.ts:       return taggedIcons.tsTest;
+			case taggedIcons.coffee:     return taggedIcons.coffeeTest;
+			case taggedIcons.go:         return taggedIcons.goTest;
+			case taggedIcons.haskell:    return taggedIcons.haskellTest;
+			case taggedIcons.js:         return taggedIcons.jsTest;
+			case taggedIcons.perl:       return taggedIcons.perlTest;
+			case taggedIcons.python:     return taggedIcons.pythonTest;
+			case taggedIcons.ruby:       return taggedIcons.rubyTest;
+			case taggedIcons.rust:       return taggedIcons.rustTest;
+			case taggedIcons.executable: return taggedIcons.genericTest;
+			case taggedIcons.ts:         return taggedIcons.tsTest;
 		}
 		return null;
 	}
@@ -49,12 +49,14 @@ class HashbangStrategy extends HeaderStrategy {
 		if("node" === name && /\.tsx?$/i.test(resource.name))
 			return null;
 		
-		let result = IconTables.matchInterpreter(name);
+		let remap, result = IconTables.matchInterpreter(name);
 		
 		// HACK: Remap icons of test-files containing hashbangs (#812)
-		const remap = this.getRemapping(result);
-		if(null !== remap && remap.match.test(resource.name))
-			return remap;
+		if(result && null !== (remap = this.getRemapping(result))){
+			const {match, matchPath} = remap;
+			if(match.test(matchPath ? resource.path : resource.name))
+				return remap;
+		}
 		
 		// Valid hashbang, unrecognised interpreter
 		if(!result){